8fe12e7ed2e6b00365c97b55176c492454059d42
WLopesF <williamlopesfreitas789@gmail.com>
2024-10-10 10:04:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+
+sum = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Sum: ", sum)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+
